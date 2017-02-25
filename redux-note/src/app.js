30c9ba913108ecd764ca91e4d2452dc0bccb5340
ushiboy <ushiboy.dev@gmail.com>
2017-02-25 22:42:10 +0900

    add flow

--- redux-note/src/app.js
+++ redux-note/src/app.js
@@ -1,7 +1,9 @@
+/* @flow */
 import React from 'react';
 import { render } from 'react-dom';
+import App from './pages/App/App';
 
 render(
-  <h1>Hello World</h1>,
+  <App />,
   document.getElementById('app')
 );
