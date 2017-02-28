295483654c70ffeed21faff92aa63ac75df75b95
ushiboy <ushiboy.dev@gmail.com>
2017-02-28 09:47:13 +0900

    add global header

--- redux-note/src/pages/App/App.js
+++ redux-note/src/pages/App/App.js
@@ -1,13 +1,19 @@
 /* @flow */
 import React from 'react';
 import Dashboard from '../Dashboard/Dashboard';
+import GlobalHeader from '../../components/GlobalHeader/GlobalHeader';
 
 export default class App extends React.Component {
 
   render() {
     return (
-      <div className="page-App-main">
-        <Dashboard />
+      <div className="page-App">
+        <div className="page-App-header">
+          <GlobalHeader />
+        </div>
+        <div className="page-App-main">
+          <Dashboard />
+        </div>
       </div>
     );
   }
