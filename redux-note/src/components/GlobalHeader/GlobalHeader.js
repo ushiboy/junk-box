c019964e89e117a783fdea311d848b74351a1478
ushiboy <ushiboy.dev@gmail.com>
2017-03-04 23:40:47 +0900

    fix global header

--- redux-note/src/components/GlobalHeader/GlobalHeader.js
+++ redux-note/src/components/GlobalHeader/GlobalHeader.js
@@ -19,20 +19,8 @@ export default class GlobalHeader extends React.Component {
           </div>
           <div id="navbar" className="navbar-collapse collapse">
             <ul className="nav navbar-nav">
-              <li className="active"><Link to="/"><span className="glyphicon glyphicon-home" aria-hidden="true"></span> Home</Link></li>
-              <li><Link to="/starred">About</Link></li>
-              <li className="dropdown">
-                <a href="#" className="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span className="caret"></span></a>
-                <ul className="dropdown-menu">
-                  <li><a href="#">Action</a></li>
-                  <li><a href="#">Another action</a></li>
-                  <li><a href="#">Something else here</a></li>
-                  <li role="separator" className="divider"></li>
-                  <li className="dropdown-header">Nav header</li>
-                  <li><a href="#">Separated link</a></li>
-                  <li><a href="#">One more separated link</a></li>
-                </ul>
-              </li>
+              <li className="active"><Link to="/"><span className="glyphicon glyphicon-dashboard" aria-hidden="true"></span> Dashboard</Link></li>
+              <li><Link to="/starred">Starred</Link></li>
             </ul>
           </div>
         </div>
